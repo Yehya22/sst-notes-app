c1c5c158753deb47a642d5352038c1478998bc42
Yehya22 <yehya.alsager@gmail.com>
2022-09-06 17:06:54 +0200

    Adding an init error_2

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -1,26 +1,41 @@
-import handler from "../util/handler";
-import dynamoDb from "../util/dynamodb";
+import { Api, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
 
-// Some faulty code
-dynamoDb.notExist();
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
 
-export const main = handler(async (event) => {
-  const params = {
-    TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
-    Key: {
-      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      noteId: event.pathParameters.id,
+  // Create the API
+  const api = new Api(stack, "Api", {
+  //   customDomain:
+  // app.stage === "prod" ? "api.my-serverless-app.com" : undefined,
+    defaults: {
+      memorySize: 128,
+      authorizer: "iam",
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+          STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+        },
+      },
     },
+    routes: {
+        "GET /notes": "functions/list.main",
+      "POST /notes": "functions/create.main",
+      "GET /notes/{id}": "functions/get.main",
+      "PUT /notes/{id}": "functions/update.main",
+      "DELETE /notes/{id}": "functions/delete.main",
+      "POST /billing": "functions/billing.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint:api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
   };
-
-  const result = await dynamoDb.get(params);
-  if (!result.Item) {
-    throw new Error("Item not found.");
-  }
-
-  // Return the retrieved item
-  return result.Item;
-});
+}
